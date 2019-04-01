c78061a6fba3446e10c77de068bb8005dc7f021f
zelta99 <zelta1990@gmail.com>
2019-04-01 13:59:29 +1300

    change db ppwd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'Xxs19900729xxs',
+        'PASSWORD':'django1234',
         'HOST':'localhost',
         'PORT':'5432',
     }
